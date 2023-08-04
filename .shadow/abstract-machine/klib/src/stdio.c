5ee180f14b3558f4e1468f3525df85303c43062c
tracer-nju <tracer@nju.edu.cn>
2023-08-04 13:45:19 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:45:19 up  5:39,  0 users,  load average: 0.22, 0.21, 0.13

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -7,7 +7,89 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buffer[256];//Be tolerant with my_printf, never try to jump ovet the band of the buffer -- buffer overflow
+ 
+  int temp = 0;
+  va_list arg;
+
+  char* p_string = NULL;
+  char* p_buffer = buffer;
+  char* p_temp   = NULL;
+
+  int counter = 0;
+  int number  = 0;
+  int foo     = 0;
+  
+  va_start(arg,fmt);
+  char fmt_copy[256];
+  strcpy(fmt_copy, fmt);
+  for(counter = 0,p_string = fmt_copy;*(p_string) != '\0';)
+  {
+          switch(*p_string)
+          {
+                  case '%':
+                          p_string++;
+
+                          switch(*p_string)
+                          {
+                                  case 'd':
+
+                                          temp = va_arg(arg,int);
+
+                                          foo = temp;
+
+                                          while(foo)
+                                          {
+                                                  number++;
+                                                  counter++;
+                                                  foo /= 10;
+                                          }
+
+                                          foo = temp;
+
+                                          while(number)
+                                          {
+                                                  *(p_buffer+number-1) = (foo%10);
+                                                  foo /= 10;
+                                                  number--;
+                                          }
+
+                                          p_buffer += number;
+                                  break;
+
+                                  case 'c':
+                                          temp = va_arg(arg,int);
+                                          *(p_buffer++) = temp;
+                                  break;
+
+                                  case 's':
+                                          p_temp = va_arg(arg,char*);
+
+                                          while(p_temp != NULL)
+                                          {
+                                                  *(p_buffer++) = *(p_temp++);
+                                                  counter++;
+                                          }
+                                  break;
+
+                                  default:
+                                          break;
+
+                          }
+                  break;
+
+                  default:
+                          *(p_buffer++) = *(p_string++);
+                          counter++;
+          }
+  }
+
+  va_end(arg);
+
+  p_buffer = NULL;
+  for (char *p = buffer; *p; p++) putch(*p);
+  return counter;
+
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
