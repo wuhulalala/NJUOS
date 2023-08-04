3fc81299f6b69fe372c3531b475f27ccd407b00a
tracer-nju <tracer@nju.edu.cn>
2023-08-04 15:35:01 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:35:01 up  7:28,  0 users,  load average: 0.29, 0.35, 0.54

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -227,9 +227,11 @@ void splash() {
         size_t srcx = ROUND(x * (IMG_WIDTH / w));
         size_t srcy = ROUND(y * (IMG_HEIGHT / h));
         pixels[x + abs_y * w] = get_pixel(srcx, srcy);
+        puts("there is question?\n");
       }
     }
     draw_tile(0, i * SIDE, w, SIDE, pixels);
+    puts("there is question?\n");
   }
 }
 
