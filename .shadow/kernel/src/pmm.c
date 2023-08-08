cc4a3270cd73d9a8323a6ff56722aeb230565adb
tracer-nju <tracer@nju.edu.cn>
2023-08-08 17:34:14 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:34:14 up 18:18,  0 users,  load average: 1.13, 1.09, 1.09

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -11,8 +11,7 @@ static void kfree(void *ptr) {
 // 框架代码中的 pmm_init (在 AbstractMachine 中运行)
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
-  putch(pmsize + '0');
-  //printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
 #else
 // 测试代码的 pmm_init ()
