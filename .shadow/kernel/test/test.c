d88c442c0d3105cfdf59b000841b63aefb9eafca
tracer-nju <tracer@nju.edu.cn>
2023-08-08 16:54:14 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:54:14 up 17:38,  0 users,  load average: 1.41, 1.24, 1.18

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -1,8 +1,8 @@
 #include "thread.h"
 #include "am.h"
+extern pmm;
 static void entry(int tid) { pmm->alloc(128); }
 static void goodbye()      { printf("End.\n"); }
-extern pmm;
 int main() {
     pmm->init();
     for (int i = 0; i < 4; i++)
