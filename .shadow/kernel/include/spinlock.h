443bb19c6ab436fae62dc9cdbd4e918208508c78
tracer-nju <tracer@nju.edu.cn>
2023-08-09 20:29:48 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:29:48 up  5:44,  0 users,  load average: 0.39, 0.29, 0.19

--- .shadow/kernel/include/spinlock.h
+++ .shadow/kernel/include/spinlock.h
@@ -1,5 +1,7 @@
 #include <am.h>
 typedef int spinlock_t;
 #define SPIN_INIT() 0
+#define LOCKED 1
+#define UNLOCKED 0
 void spin_lock(spinlock_t *lk);
 void spin_unlock(spinlock_t *lk);
--- .shadow/kernel/src/spinlock.c
+++ .shadow/kernel/src/spinlock.c
@@ -1,20 +1,16 @@
-
 #include "spinlock.h"
-//static inline int atomic_xchg(volatile int *addr, int newval) {
-    //int result;
-    //asm volatile ("lock xchg %0, %1":
-    //"+m"(*addr), "=a"(result) : "1"(newval) : "memory");
-    //return result;
-//}
-
 void spin_lock(spinlock_t *lk) {
     while (1) {
-        intptr_t value = atomic_xchg(lk, 1);
-        if (value == 0) {
+        intptr_t value = atomic_xchg(lk, LOCKED);
+        if (value == UNLOCKED) {
             break;
         }
     }
 }
 void spin_unlock(spinlock_t *lk) {
-    atomic_xchg(lk, 0);
+    panic_on(atomic_xchg(lk, UNLOCKED) != LOCKED, "Failed to release lock\n");
+}
+
+intptr_t try_lock(spinlock_t *lk) {
+    return atomic_xchg(lk, LOCKED);
 }
