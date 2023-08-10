ca0ca7f9d6569950fbdd08a050ad427a67361256
tracer-nju <tracer@nju.edu.cn>
2023-08-10 20:44:09 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:44:09 up  3:33,  0 users,  load average: 0.06, 0.13, 0.11

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -52,6 +52,8 @@ static void pmm_init() {
   chunks_size = (((uintptr_t)heap.end - (uintptr_t)heap.start) + PGSIZE - 1) / PGSIZE;
   printf("the address is in the %d page\n", ((uintptr_t)ROUNDUP(heap.start + PGSIZE, PGSIZE) - (uintptr_t)heap.start) / PGSIZE);
   chunks[1] = 0xfffffffffff0;
+  uintptr_t temp = 0xffffffffffff & CHUNKS_FLAG_MASK;
+  printf("the temp flag is %p\n", temp);
   printf("the chunk is %p\n", chunks[1]);
   printf("the idx mask is %p, the flag mask is %p, the status flag is %p\n", CHUNKS_IDX_MASK, CHUNKS_FLAG_MASK, CHUNKS_STATUS_MASK);
   printf("idx is %p, flag is %ld, status is %ld\n", CHUNKS_GET_IDX(chunks[1]), CHUNKS_GET_FLAG(chunks[1]), CHUNKS_GET_STATUS(chunks[1]));
