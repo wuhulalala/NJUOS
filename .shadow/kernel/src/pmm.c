e85629ce73e2c6ebbb83e4493471fa99a12a2c44
tracer-nju <tracer@nju.edu.cn>
2023-08-10 21:18:23 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:18:23 up  4:07,  0 users,  load average: 0.32, 0.19, 0.13

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -54,7 +54,7 @@ static void pmm_init() {
   chunks[1] = 0xfffffffffffffff0;
   printf("the chunk is %p\n", chunks[1]);
   printf("the idx mask is %p, the flag mask is %p, the status flag is %p\n", CHUNKS_IDX_MASK, CHUNKS_FLAG_MASK, CHUNKS_STATUS_MASK);
-  printf("idx is %p, flag is %ld, status is %ld\n", CHUNKS_GET_IDX(chunks[1]), CHUNKS_GET_FLAG(chunks[1]), CHUNKS_GET_STATUS(chunks[1]));
+  printf("idx is %p, flag is %p, status is %p\n", CHUNKS_GET_IDX(chunks[1]), CHUNKS_GET_FLAG(chunks[1]), CHUNKS_GET_STATUS(chunks[1]));
 
   printf("the index is %p, the flag is %p, the status is %p\n", CHUNKS_GETIDX_ADD(chunks + 10), CHUNKS_GETFLAG_ADD(chunks + 10), CHUNKS_GETSTATUS_ADD(chunks + 10));
   CHUNKS_SETIDX_ADD((uintptr_t)chunks + (uintptr_t)10, 100);
