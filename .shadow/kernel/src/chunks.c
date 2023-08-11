6259db18204424579147eba0c474187ed9811a7a
tracer-nju <tracer@nju.edu.cn>
2023-08-11 21:06:23 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:06:23 up  9:48,  0 users,  load average: 0.02, 0.07, 0.08

--- .shadow/kernel/src/chunks.c
+++ .shadow/kernel/src/chunks.c
@@ -2,9 +2,10 @@
 //find the minimum value i satisfied 2^i >= n
 int log_n(size_t n) {
     int result = 0;
+    uintptr_t temp = 0;
     assert(n <= MAXSIZE && n >= 0);
     for (int i = 0; i < sizeof(uintptr_t) * 8; i++) {
-        if (((uintptr_t)1 << i) >= n) {
+        if ((temp = ((uintptr_t)1 << i)) >= n) {
             result = i;
             break;
         }
