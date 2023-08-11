04fe656d1145c66a67c16d52f4ef91e0484db65a
tracer-nju <tracer@nju.edu.cn>
2023-08-11 14:17:50 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:17:50 up  3:00,  0 users,  load average: 0.11, 0.08, 0.11

--- .shadow/kernel/src/spinlock.c
+++ .shadow/kernel/src/spinlock.c
@@ -1,5 +1,11 @@
 #include "spinlock.h"
 #include <klib-macros.h>
+static inline int atomic_xchg(volatile int *addr, int newval) {
+    int result;
+    asm volatile ("lock xchg %0, %1":
+    "+m"(*addr), "=a"(result) : "1"(newval) : "memory");
+    return result;
+}
 void spin_lock(spinlock_t *lk) {
     while (1) {
         intptr_t value = atomic_xchg(lk, LOCKED);
