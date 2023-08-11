b208a5fabdde0dac255fb6daf45f8878ce1d899b
tracer-nju <tracer@nju.edu.cn>
2023-08-11 14:45:23 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:45:23 up  3:27,  0 users,  load average: 0.22, 0.29, 0.26

--- .shadow/kernel/src/chunks.c
+++ .shadow/kernel/src/chunks.c
@@ -2,7 +2,7 @@
 //find the minimum value i satisfied 2^i >= n
 int log_n(size_t n) {
     int result = 0;
-    panic_on(n > MAXSIZE || n < 0, "the invalid memory request");
+    assert(n <= MAXSIZE && n >= 0);
     for (int i = 0; i < sizeof(uintptr_t) * 8; i++) {
         if (((uintptr_t)1 << i) >= n) {
             break;
