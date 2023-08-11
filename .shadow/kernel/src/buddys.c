eb39f873248b7ee1dccc973bc29c91982a56ec28
tracer-nju <tracer@nju.edu.cn>
2023-08-11 17:05:36 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:05:36 up  5:47,  0 users,  load average: 0.73, 0.58, 0.43

--- .shadow/kernel/src/buddys.c
+++ .shadow/kernel/src/buddys.c
@@ -18,6 +18,10 @@ void buddys_init() {
         assert(iter);
         list_insert((Chunk*)iter);
     }
+    for (uintptr_t iter = (uintptr_t)chunks_base; iter + MAXSIZE < (uintptr_t)heap.end; iter += MAXSIZE) {
+        assert(iter);
+        list_remove((Chunk*)iter);
+    }
     printf("buddys initial finished \n");
     
 }
@@ -36,12 +40,12 @@ void list_insert(Chunk* chunk) {
         break;
     }
     Chunk *next = head -> next;
-    //spin_lock(buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
+    spin_lock(&buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
     head -> next = chunk;
     chunk -> prev = head;
     next -> prev = chunk;
     chunk -> next = next;
-    //spin_unlock(buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
+    spin_unlock(&buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
 
     int count = 0;
     for (Chunk *p = head -> next; p != head; p = p -> next, count ++) {
@@ -54,10 +58,15 @@ void list_insert(Chunk* chunk) {
 void list_remove(Chunk *chunk) {
     assert(chunk -> next != chunk);
     Chunk *next = chunk -> next, *prev = chunk -> prev;
-    //spin_lock(buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
+    spin_lock(&buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
     prev -> next = next;
     next -> prev = prev;
-    //spin_unlock(buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
+    spin_unlock(&buddys[CHUNKS_GET_IDX_ADD(chunk)].lk);
     assert(prev -> next);
     assert(next -> prev);
+    int count = 0;
+    for (Chunk *p = chunk -> next; p != chunk; p = p -> next, count ++) {
+        printf("this is the %d node of the list, the address is %p\n", count, (void *)p);
+    }
+    printf("------------------------------------\n");
 }
