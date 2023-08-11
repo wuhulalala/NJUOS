ef8aba295e08bf21e817ba2a8d7ba5d95a1e2377
tracer-nju <tracer@nju.edu.cn>
2023-08-11 19:58:19 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:58:19 up  8:40,  0 users,  load average: 0.12, 0.12, 0.11

--- .shadow/kernel/src/buddys.c
+++ .shadow/kernel/src/buddys.c
@@ -8,11 +8,7 @@ void buddys_init() {
     chunks_base = (uintptr_t*)ROUNDUP((uintptr_t)buddys + (uintptr_t)(buddys_size * sizeof(Chunk)), MAXSIZE);
     for (int i = 0; i < buddys_size; i++) {
         buddys[i].next = buddys[i].prev = &buddys[i];
-        #ifdef TEST
-        buddys[i].lk = PTHREAD_MUTEX_INITIALIZER;
-        #else
         buddys[i].lk = SPIN_INIT();
-        #endif
         
     }
 
