ec71197e02d10746378f05ac1479c9331b4f4921
tracer-nju <tracer@nju.edu.cn>
2023-08-11 20:32:22 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:32:22 up  9:14,  0 users,  load average: 0.59, 0.35, 0.24

--- .shadow/kernel/include/spinlock.h
+++ .shadow/kernel/include/spinlock.h
@@ -7,9 +7,9 @@ typedef int spinlock_t;
 #define SPIN_INIT() 0
 #define LOCKED 1
 #define UNLOCKED 0
-void spin_lock(spinlock_t *lk);
-void spin_unlock(spinlock_t *lk);
-uintptr_t try_lock(spinlock_t *lk); 
+//void spin_lock(spinlock_t *lk);
+//void spin_unlock(spinlock_t *lk);
+//uintptr_t try_lock(spinlock_t *lk); 
 #else
 #define SPIN_INIT() PTHREAD_MUTEX_INITIALIZER
 #include <stdint.h>
--- .shadow/kernel/src/spinlock.c
+++ .shadow/kernel/src/spinlock.c
@@ -1,20 +1,20 @@
 #include "spinlock.h"
-//#ifndef TEST
-//#include <klib-macros.h>
-//void spin_lock(spinlock_t *lk) {
-    //while (1) {
-        //uintptr_t value = atomic_xchg(lk, LOCKED);
-        //if (value == UNLOCKED) {
-            //break;
-        //}
-    //}
-//}
-//void spin_unlock(spinlock_t *lk) {
-    //assert(atomic_xchg(lk, UNLOCKED) == LOCKED);
-//}
+#ifndef TEST
+#include <klib-macros.h>
+void spin_lock(spinlock_t *lk) {
+    while (1) {
+        uintptr_t value = atomic_xchg(lk, LOCKED);
+        if (value == UNLOCKED) {
+            break;
+        }
+    }
+}
+void spin_unlock(spinlock_t *lk) {
+    assert(atomic_xchg(lk, UNLOCKED) == LOCKED);
+}
 
-//uintptr_t try_lock(spinlock_t *lk) {
-    //return atomic_xchg(lk, LOCKED);
-//}
+uintptr_t try_lock(spinlock_t *lk) {
+    return atomic_xchg(lk, LOCKED);
+}
 
-//#endif
+#endif
