07c96129949660d65a12d582ffb6e22421bb12be
tracer-nju <tracer@nju.edu.cn>
2023-08-12 23:55:46 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:55:46 up  2:58,  0 users,  load average: 0.17, 0.22, 0.23

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -3,26 +3,43 @@
 #include <stdlib.h>
 #include <time.h>
 #define PGSIZE 4096
-static void entry(int tid) { 
+#define NTHREAD 4
+static void entry1(int tid) { 
     srand(time(NULL));
-    for (int i = 0; i < 10; i++) {
-        int random = rand() % 4096 + 1;
-        int random_mem = (random - 1) * PGSIZE + random;
+    for (int i = 0; i < 1000000; i++) {
+        int random = rand() % 4095 + 1;
+        int random_mem = (random) * PGSIZE + random;
         char *mem = pmm->alloc(random_mem); 
         assert(mem);
         pmm->free(mem); 
 
     }
 }
-static void goodbye() { 
-    printf("End.\n"); 
+
+static void entry2(int tid) { 
+    srand(time(NULL));
+    for (int i = 0; i < 1000000; i++) {
+        int random = rand() % 16 + 1;
+        int random_mem = (random) * PGSIZE + random;
+        char *mem = pmm->alloc(random_mem); 
+        assert(mem);
+        pmm->free(mem); 
+
+    }
 }
+
+static void goodbye() { 
+    printf("goodbye.\n"); 
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) exit(1);
     pmm->init();
     switch(atoi(argv[1])) {
         case 0: do_test_0();
         case 1: do_test_1();
+        case 1: do_test_2();
+        case 1: do_test_3();
 }
 }
 void do_test_0() {
@@ -62,4 +79,20 @@ void do_test_1() {
 
     }
     printf("End\n");
+}
+
+void do_test_2() {
+
+    for (int t = 0; t < NTHREAD; t++) {
+        create(entry1);
+    }
+    join(goodbye);
+}
+
+void do_test_3() {
+
+    for (int t = 0; t < NTHREAD; t++) {
+        create(entry2);
+    }
+    join(goodbye);
 }
