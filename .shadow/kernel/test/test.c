633cd3828e9cd25b4bf23e8a2e9e4b426c128881
tracer-nju <tracer@nju.edu.cn>
2023-08-12 21:28:42 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:28:42 up 31 min,  0 users,  load average: 0.20, 0.25, 0.18

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -1,13 +1,25 @@
 #include "thread.h"
 #include "kernel.h"
+#include <stdlib.h>
+#include <time.h>
 #define PGSIZE 4096
-static void entry(int tid) { pmm->alloc(5 * PGSIZE); }
-static void goodbye()      { printf("End.\n"); }
+static void entry(int tid) { 
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        int random = rand() % 4096 + 1;
+        int random_mem = (random - 1) * PASIZE + random;
+        char *mem = pmm->alloc(random_mem); 
+        pmm->free(mem); 
+
+    }
+}
+static void goodbye() { 
+    printf("End.\n"); 
+}
 int main() {
     pmm->init();
-    pmm->alloc(5 * PGSIZE);
     printf("End\n");
-    //for (int i = 0; i < 4; i++)
-    //create(entry);
-    //join(goodbye);
+    for (int i = 0; i < 4; i++)
+    create(entry);
+    join(goodbye);
 }
