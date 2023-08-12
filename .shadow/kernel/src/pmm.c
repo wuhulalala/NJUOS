aae6960552f6d54b9457bd046e88fcebe6970219
tracer-nju <tracer@nju.edu.cn>
2023-08-12 23:34:35 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:34:35 up  2:37,  0 users,  load average: 0.20, 0.21, 0.20

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -21,7 +21,7 @@ void buddys_free(uintptr_t * pointer);
 
 static void *kalloc(size_t size) {
   uintptr_t *pointer = NULL;
-  if (size >= 4 * PGSIZE) {
+  if (size >= PGSIZE) {
     pointer = buddys_malloc(size);
   }
   //assert(pointer);
