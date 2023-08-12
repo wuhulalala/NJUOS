79b505eb8a623db376f760a878936940ad21fdaa
tracer-nju <tracer@nju.edu.cn>
2023-08-12 23:45:22 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:45:22 up  2:48,  0 users,  load average: 0.04, 0.17, 0.21

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -17,8 +17,15 @@ static void entry(int tid) {
 static void goodbye() { 
     printf("End.\n"); 
 }
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) exit(1);
     pmm->init();
+    switch(atoi(argv[1])) {
+        case 0: do_test_0();
+        case 1: do_test_1();
+}
+}
+void do_test_0() {
     srand(time(NULL));
     for (int i = 0; i < 1000000; i++) {
         int random = rand() % 4095 + 1;
@@ -33,8 +40,26 @@ int main() {
         printf("finished %d round\n", i + 1);
 
     }
-    printf("End\n");
+    printf("test 0 finished\n");
     //for (int i = 0; i < 4; i++)
     //create(entry);
     //join(goodbye);
+}
+void do_test_1() {
+
+    srand(time(NULL));
+    for (int i = 0; i < 1000000; i++) {
+        int random = rand() % 16 + 1;
+        int random_mem = (random) * PGSIZE + random;
+        printf("malloc %d page memory\n", random_mem / 4096);
+        char *mem = pmm->alloc(random_mem); 
+        if (!mem) {
+            printf("the random_mem is %d\n", random_mem);
+        }
+        pmm->free(mem); 
+        //printf("free %d page memory\n", random_mem / 4096);
+        printf("finished %d round\n", i + 1);
+
+    }
+    printf("End\n");
 }
