03d435eb5e96811311b4ff8043151639389001f1
tracer-nju <tracer@nju.edu.cn>
2023-08-13 12:32:45 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:32:45 up  2:50,  0 users,  load average: 0.09, 0.15, 0.28

--- .shadow/kernel/src/buddys.c
+++ .shadow/kernel/src/buddys.c
@@ -98,10 +98,13 @@ uintptr_t *buddys_malloc(size_t n) {
     assert(idx >= 0);
     Chunk *head = NULL, *pointer = NULL;
     while (idx < buddys_size) {
-        //spin_lock(&buddys[idx].lk);
+        spin_lock(&buddys[idx].lk);
         head = &buddys[idx];
-        if (head != head -> next) break;
-        //spin_unlock(&buddys[idx].lk);
+        if (head != head -> next) {
+            spin_unlock(&buddys[idx].lk);
+            break;
+        }
+        spin_unlock(&buddys[idx].lk);
         idx++;
     }
     if (idx == buddys_size) {
