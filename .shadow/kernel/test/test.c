d9eee2b646741979cedddebcf00d433290efa482
tracer-nju <tracer@nju.edu.cn>
2023-08-14 13:47:52 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:47:52 up  3:28,  0 users,  load average: 0.21, 0.23, 0.14

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -125,28 +125,24 @@ void do_buddy_test_3() {
 
 void do_slab_test_0() {
     srand(time(NULL));
-    char *mem = pmm -> alloc(790);
+    char *mem = pmm -> alloc(1989);
     pmm -> free(mem);
-    mem = pmm -> alloc(3685);
-    pmm -> free(mem);
-    mem = pmm -> alloc(790);
-    pmm -> free(mem);
-    mem = pmm -> alloc(2839);
+    mem = pmm -> alloc(77);
     pmm -> free(mem);
 
     
-    for (int i = 0; i < 1000000; i++) {
-        int random = rand() % (PGSIZE - MINSIZE) + MINSIZE;
-        printf("malloc %d bytes memory\n", random);
-        char *mem = pmm->alloc(random); 
-        if (!mem) {
-            printf("the random_mem is %d\n", random);
-        }
-        pmm->free(mem); 
-        printf("free %d bytes memory\n", random);
-        printf("finished %d round\n", i + 1);
+    //for (int i = 0; i < 1000000; i++) {
+        //int random = rand() % (PGSIZE - MINSIZE) + MINSIZE;
+        //printf("malloc %d bytes memory\n", random);
+        //char *mem = pmm->alloc(random); 
+        //if (!mem) {
+            //printf("the random_mem is %d\n", random);
+        //}
+        //pmm->free(mem); 
+        //printf("free %d bytes memory\n", random);
+        //printf("finished %d round\n", i + 1);
 
-    }
+    //}
     printf("End\n");
 }
 
