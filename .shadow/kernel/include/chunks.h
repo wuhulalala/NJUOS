0c21ecdc42b77a97dcbabe6af8d9ae4c0784ee21
tracer-nju <tracer@nju.edu.cn>
2023-08-14 14:43:55 +0800

    Linux liscopye 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:43:55 up  4:24,  0 users,  load average: 0.00, 0.04, 0.06

--- .shadow/kernel/include/chunks.h
+++ .shadow/kernel/include/chunks.h
@@ -67,7 +67,7 @@ spinlock_t lk;
 #define CHUNKS_SET_FLAG(val, new_value) ((((val) << 1) >> 1) | (uintptr_t)(new_value) << (CHUNKS_IDX_SIZE + CHUNKS_STATUS_SIZE))
 #define CHUNKS_SET_STATUS(val, new_value) ((((((val) << 1) >> 1) >> CHUNKS_IDX_SIZE) == (uintptr_t)(new_value)) ? (val) : (val) ^ CHUNKS_STATUS_MASK)
 
-#define CHUNKS_AC_INDEX(add) (((uintptr_t)ROUNDUP(add, PGSIZE + 1) - (uintptr_t)chunks_base) / PGSIZE)
+#define CHUNKS_AC_INDEX(add) (((uintptr_t)ROUNDUP(add, PGSIZE) - (uintptr_t)chunks_base) / PGSIZE)
 
 #define CHUNKS_GET_IDX_ADD(add) CHUNKS_GET_IDX(chunks[CHUNKS_AC_INDEX(add)])
 #define CHUNKS_GET_FLAG_ADD(add) CHUNKS_GET_FLAG(chunks[CHUNKS_AC_INDEX(add)])
