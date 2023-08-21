354eac8b8390fbe3f877babc85c4afffa5e9c0ab
tracer-nju <tracer@nju.edu.cn>
2023-08-21 20:14:30 +0800

    Linux liscopye 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:14:30 up 21:45,  1 user,  load average: 0.93, 0.78, 0.61

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -19,10 +19,8 @@ static void os_run() {
 }
 
 static Context * os_trap(Event ev, Context *context) {
-  putch('a');
-  putch('\n');
 
-  //printf("cpu %d : Hello World\n", cpu_current());
+  printf("cpu %d : Hello World\n", cpu_current());
   return context;
 }
 static void os_on_irq(int seq, int event, handler_t handler) {
