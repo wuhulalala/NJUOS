eaae272ae4b9f7fae15675a3355d4680078f5284
tracer-nju <tracer@nju.edu.cn>
2023-08-24 21:13:29 +0800

    Linux liscopye 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:13:29 up 1 day,  9:55,  1 user,  load average: 0.92, 0.92, 0.81

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -209,12 +209,16 @@ void kmt_schedule() {
             panic("error status");
         }
 
+        printf("is here!!!\n");
         task_t *p = task -> next;
-        while (p != READY) {p = p -> next;}
+        while (p != READY) {p = p -> next;
+            printf("is ready!!!\n");
+        }
         p -> round = KMT_INIT_ROUND;
         p -> status = WAIT_TO_LOAD;
         check_static_fence(p);
         
+        printf("is here!!!\n");
 
         current_task[cpu] = p;
         kmt -> spin_unlock(&task_lk);
