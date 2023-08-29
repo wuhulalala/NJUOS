0493cbdf180de112b189d18e233e0dcd925cc4fb
tracer-nju <tracer@nju.edu.cn>
2023-08-29 19:42:17 +0800

    Linux liscopye 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:42:17 up 27 min,  1 user,  load average: 0.24, 0.42, 0.52

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -103,9 +103,9 @@ Context *irq_time_handler(Event ev, Context *context) {
     {
     case RUNNING:
 
-        //printf("hello world\n");
-        //if (--(task -> round) < 0) 
-            //task -> status = WAIT_TO_SCHEDULE;
+        printf("hello world\n");
+        if (--(task -> round) < 0) 
+            task -> status = WAIT_TO_SCHEDULE;
 
         break;
     
