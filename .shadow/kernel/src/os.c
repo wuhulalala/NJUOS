ade22d8321abf84fedfe1bbf3d1fedbca933856e
tracer-nju <tracer@nju.edu.cn>
2023-08-30 23:43:21 +0800

    Linux liscopye 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:43:21 up 12:35,  1 user,  load average: 1.58, 1.33, 0.98

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,5 +1,6 @@
 #include <os.h>
 #include <common.h>
+#include <devices.h>
 extern spinlock_t irq_lk;
 void kmt_schedule();
 
@@ -7,10 +8,25 @@ void kmt_schedule();
 
 
 
+static void tty_reader(void *arg) {
+  device_t *tty = dev->lookup(arg);
+  char cmd[128], resp[128], ps[16];
+  snprintf(ps, 16, "(%s) $ ", arg);
+  while (1) {
+    tty->ops->write(tty, 0, ps, strlen(ps));
+    int nread = tty->ops->read(tty, 0, cmd, sizeof(cmd) - 1);
+    cmd[nread] = '\0';
+    sprintf(resp, "tty reader task: got %d character(s).\n", strlen(cmd));
+    tty->ops->write(tty, 0, resp, strlen(resp));
+  }
+}
+
 static void os_init() {
   pmm -> init();
   kmt -> init();
   dev -> init();
+  kmt->create(pmm->alloc(sizeof(task_t)), "tty_reader", tty_reader, "tty1");
+  kmt->create(pmm->alloc(sizeof(task_t)), "tty_reader", tty_reader, "tty2");
   
 }
 
